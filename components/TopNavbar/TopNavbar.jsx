b92c556527397158ab3980f3051116dbe8ac13f1
Oğuzhan YALÇIN <oguzhan@post.com>
2025-07-05 16:33:09 +0300

    ddd

--- components/TopNavbar/TopNavbar.jsx
+++ components/TopNavbar/TopNavbar.jsx
@@ -1,7 +1,9 @@
 "use client"
 import { Navbar, NavbarBrand, NavbarContent } from "@heroui/navbar";
+import { Button } from "@heroui/button";
 import { settings } from "@/config/settings";
 import { usePathname } from 'next/navigation'
+import { GeriIcon } from "@/public/icon.jsx"
 import Styles from "./style.module.css"
 
 export default function TopNavbar() {
@@ -10,19 +12,23 @@ export default function TopNavbar() {
 
     return (
         <>
-            <Navbar className={`${Styles.navbar} h-12`} position="sticky">
+            <Navbar className={`${Styles.navbar} h-12 flex justify-between `} position="sticky">
                 {
                     pathname != "/" ? (
-                        <NavbarBrand>
-                            
-                        </NavbarBrand>
+                            <Button isIconOnly className="w-10"  aria-label="geri" variant="light" color="primary">
+                                <GeriIcon />
+                            </Button>
 
                     ) : ""
                 }
-                <NavbarContent>
-                    <p className="font-bold text-2xl tracking-wider text-inherit text-center m-auto">{settings.title}</p>
-                </NavbarContent>
+                    <NavbarContent className="m-auto">
+                        <p className="font-bold text-2xl tracking-wider text-inherit text-center m-auto w-max">{settings.title}</p>
+                    </NavbarContent>
+                {
+                    pathname != "/" ? (<div className="size-10" />) : ""
+                }
             </Navbar>
+
 
         </>
     )
