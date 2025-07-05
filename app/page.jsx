f7904469bf6fc79e612b8af6d96cad16e2719cdb
Oğuzhan YALÇIN <oguzhan@post.com>
2025-07-06 02:00:02 +0300

    select-none

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <>
     <TopNavbar pathname="/" />
     <Banner />
-    <div className="grid grid-cols-2 justify-center gap-4 max-w-fit mx-3 m-auto max-h-fit mt-5 ">
+    <div className="grid grid-cols-2 justify-center gap-4 max-w-fit mx-3 m-auto max-h-fit mt-5 select-none ">
       {
         kategoriler.map((kategori) =>
            (<Kategori key={kategori.id} id={kategori.id} title={kategori.isim} image={kategori.resim} />))
